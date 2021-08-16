fa3b73c3af33ab75a2b86b4d468fc7da8f0944ac
matainsfr5sfr5sfro <88651908+matainsfr5sfr5sfro@users.noreply.github.com>
2021-08-16 11:15:44 +0300

    Create combo.py

---
+++ combo.py
@@ -0,0 +1,29 @@
+   import os,sys,random
+
+logo = """
+ ▄████▄   ▒█████   ███▄ ▄███▓ ▄▄▄▄    ▒█████  
+▒██▀ ▀█  ▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▒██▒  ██▒
+▒▓█    ▄ ▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒██░  ██▒
+▒▓▓▄ ▄██▒▒██   ██░▒██    ▒██ ▒██░█▀  ▒██   ██░
+▒ ▓███▀ ░░ ████▓▒░▒██▒   ░██▒░▓█  ▀█▓░ ████▓▒░
+░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░   ░  ░░▒▓███▀▒░ ▒░▒░▒░ 
+  ░  ▒     ░ ▒ ▒░ ░  ░      ░▒░▒   ░   ░ ▒ ▒░ 
+░        ░ ░ ░ ▒  ░      ░    ░    ░ ░ ░ ░ ▒  
+░ ░          ░ ░         ░    ░          ░ ░  
+░                                  ░          
+"""
+print(logo)
+print("\033[1;92mFile Save Combo.txt")
+print("1- Combo Maker")
+input("halbzhera : ")
+for iii in range(100000):
+    r1=random.randint(1000000, 9999999)
+    r2=random.randint(1000000, 9999999)
+    r3=random.randint(1000000, 9999999)
+    r4=random.randint(1000000, 9999999)
+    r5=random.randint(1000000, 9999999)
+    r6=random.randint(1000000, 9999999)
+    r7=random.randint(1000000, 9999999)
+    sys.stdout=open("BNX.txt","a")
+    print("+964770"+str(r1)+":0770"+str(r1))
+    print("+964770"+str(r2)+":0770"+str(r2)) 
